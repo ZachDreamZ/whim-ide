19e670b388ae3618125c5298937e0969bac5df25
Whim IDE Dev <dev@whim-ide.local>
2026-07-18 14:03:46 +0800

    chore: bump version to 0.4.5

--- src/lib/bridge.ts
+++ src/lib/bridge.ts
@@ -175,7 +175,7 @@ export type NativeBrowserState = {
 
 // App version synced with Cargo.toml
 // Keep in sync when bumping.
-export const APP_VERSION = "0.4.4";
+export const APP_VERSION = "0.4.5";
 
 export type AppSettings = {
   version: number;
